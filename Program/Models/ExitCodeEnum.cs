eee87967044432ead021d54eb37ba0a670ab32f5
Onur <onur.bolukbas@navlungo.com>
2021-07-05 20:50:57 +0300

    remove unneccessary enums

--- Program/Models/ExitCodeEnum.cs
+++ Program/Models/ExitCodeEnum.cs
@@ -3,8 +3,6 @@ namespace Program.Models
     public enum ExitCodeEnum
     {
         Success = 0,
-        PossibleCrash = 1,
-        PossibleFillOut = 2,
-        WrongInput = 3,
+        WrongInput = 1,
     }
 }
