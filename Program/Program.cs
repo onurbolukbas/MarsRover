7a8b25abb78052fc0c26d7d869f4fe55e2deb64a
Onur <onur.bolukbas@navlungo.com>
2021-07-05 18:19:51 +0300

    update program

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,6 +2,7 @@
 using Program.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Program.Helpers;
 
 namespace Program
 {
@@ -9,54 +10,75 @@ namespace Program
     {
         //dotnet run "5 5" "1 2 N" "LMLMLMLMM"
         //dotnet run "5 5" "1 2 N" "LMLMLMLMM" "3 3 E" "MMRMMRMRRM"
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            if (args == null || args.Length < 3 || args.Length % 2 == 0)
+            bool isValidInput = ProgramHelper.CheckInput(args);
+            if (!isValidInput)
             {
                 Console.WriteLine("Lutfen uygun girdiler giriniz");
+                return (int)ExitCodeEnum.WrongInput;
             }
-            else
+
+            List<Rover> roverList = new();
+
+            char[] plateauArr = args[0].ToCharArray();
+            Plateau plateau = new((int)Char.GetNumericValue(plateauArr[0]), (int)Char.GetNumericValue(plateauArr[2]));
+
+            for (int i = 1; i < args.Length; i += 2)
             {
-                List<Rover> roverList = new();
+                char[] roverStateArr = args[i].ToCharArray();
+                string name = $"Rover - {i}";
 
-                char[] plateauArr = args[0].ToCharArray();
-                Plateau plateau = new((int)plateauArr[0], (int)plateauArr[2]);
+                Rover rover = new(name,
+                (int)Char.GetNumericValue(roverStateArr[0]),
+                (int)Char.GetNumericValue(roverStateArr[2]),
+                roverStateArr[4]);
 
-                for (int i = 1; i < args.Length; i += 2)
+                roverList.Add(rover);
+            }
+
+            for (int i = 2; i < args.Length; i += 2)
+            {
+                char[] directions = args[i].ToCharArray();
+                int index = 0;
+                for (int j = 0; j < directions.Length; j++)
                 {
-
-                    char[] roverStateArr = args[i].ToCharArray();
-                    string name = $"Rover{i}";
-
-                    Rover rover = new(name,
-                    (int)Char.GetNumericValue(roverStateArr[0]),
-                    (int)Char.GetNumericValue(roverStateArr[2]),
-                    roverStateArr[4]);
-
-                    roverList.Add(rover);
-
-                }
-                for (int i = 2; i < args.Length; i += 2)
-                {
-
-                    char[] directions = args[i].ToCharArray();
-                    for (int j = 0; j < directions.Length; j++)
+                    index = (i / 2) - 1;
+                    if (directions[j].Equals('L'))
+                        roverList.ElementAt(index).State.TurnLeft();
+                    else if (directions[j].Equals('R'))
+                        roverList.ElementAt(index).State.TurnRight();
+                    else if (directions[j].Equals('M'))
                     {
-                        if (directions[j].Equals('L'))
-                            roverList.ElementAt((i / 2) - 1).State.TurnLeft();
-                        else if (directions[j].Equals('R'))
-                            roverList.ElementAt((i / 2) - 1).State.TurnRight();
-                        else if (directions[j].Equals('M'))
-                            roverList.ElementAt((i / 2) - 1).State.Move();
-
+                        if (roverList.ElementAt(index).State.IsWrongMove(plateau))
+                        {
+                            Console.WriteLine("PossibleCrash");
+                            return (int)ExitCodeEnum.PossibleCrash;
+                        }
+                        else
+                        {
+                            if (roverList.ElementAt(index).State.IsOutsideOfPlateau(plateau))
+                            {
+                                Console.WriteLine("PossibleFillOut");
+                                return (int)ExitCodeEnum.PossibleFillOut;
+                            }
+                        }
+                        roverList.ElementAt(index).State.Move();
                     }
                 }
-                foreach (var rover in roverList)
-                {
-                    Console.WriteLine($"{rover.State.Coordinate.Latitude} {rover.State.Coordinate.Longitude} {rover.State.Direction}");
-                }
+                Coordinate takenPoint = new(roverList.ElementAt(index).State.Coordinate.Latitude,
+                roverList.ElementAt(index).State.Coordinate.Longitude);
+
+                plateau.TakenPoints.Add(takenPoint);
             }
+            foreach (var rover in roverList)
+            {
+                Console.WriteLine($"{rover.State.Coordinate.Latitude} {rover.State.Coordinate.Longitude} {rover.State.Direction}");
+            }
+            return (int)ExitCodeEnum.Success;
+
 
         }
+        static int Sum(int a, int b) { return a + b; }
     }
 }
