93eea138ffec8f89ee292237ed4544747ada286f
Onur <onur.bolukbas@navlungo.com>
2021-07-05 18:19:06 +0300

    update if conditions with switch cases

--- Program/Models/RoverState.cs
+++ Program/Models/RoverState.cs
@@ -1,4 +1,5 @@
 using Program.Helpers;
+using System;
 
 namespace Program.Models
 {
@@ -13,21 +14,100 @@ namespace Program.Models
             CompassEnum direction
         )
         {
-            Coordinate = new();
-            this.Coordinate.Latitude = Latitude;
-            this.Coordinate.Longitude = Longitude;
+            Coordinate = new(Latitude, Longitude);
             this.Direction = direction;
         }
         public void Move()
         {
-            if (this.Direction == CompassEnum.North)
-                this.Coordinate.Longitude += 1;
-            if (this.Direction == CompassEnum.East)
-                this.Coordinate.Latitude += 1;
-            if (this.Direction == CompassEnum.South)
-                this.Coordinate.Longitude -= 1;
-            if (this.Direction == CompassEnum.West)
-                this.Coordinate.Latitude -= 1;
+            switch (this.Direction)
+            {
+                case CompassEnum.North:
+                    this.Coordinate.Longitude += 1;
+                    break;
+                case CompassEnum.East:
+                    this.Coordinate.Latitude += 1;
+                    break;
+                case CompassEnum.South:
+                    this.Coordinate.Longitude -= 1;
+                    break;
+                case CompassEnum.West:
+                    this.Coordinate.Latitude -= 1;
+                    break;
+            }
+        }
+        public bool IsWrongMove(Plateau plateau)
+        {
+            Coordinate coordinate;
+            switch (this.Direction)
+            {
+                case CompassEnum.North:
+                    coordinate = new(this.Coordinate.Latitude, this.Coordinate.Longitude + 1);
+                    return this.CheckPossibleCrash(plateau, coordinate);
+                case CompassEnum.East:
+                    coordinate = new(this.Coordinate.Latitude + 1, this.Coordinate.Longitude);
+                    return this.CheckPossibleCrash(plateau, coordinate);
+                case CompassEnum.South:
+                    coordinate = new(this.Coordinate.Latitude, this.Coordinate.Longitude - 1);
+                    return this.CheckPossibleCrash(plateau, coordinate);
+                case CompassEnum.West:
+                    coordinate = new(this.Coordinate.Latitude - 1, this.Coordinate.Longitude);
+                    return this.CheckPossibleCrash(plateau, coordinate);
+                default:
+                    return false;
+            }
+        }
+        public bool IsOutsideOfPlateau(Plateau plateau)
+        {
+            Coordinate coordinate;
+            switch (this.Direction)
+            {
+                case CompassEnum.North:
+                    coordinate = new(this.Coordinate.Latitude, this.Coordinate.Longitude + 1);
+                    return this.CheckPossibleOutsite(plateau, coordinate);
+                case CompassEnum.East:
+                    coordinate = new(this.Coordinate.Latitude + 1, this.Coordinate.Longitude);
+                    return this.CheckPossibleOutsite(plateau, coordinate);
+                case CompassEnum.South:
+                    coordinate = new(this.Coordinate.Latitude, this.Coordinate.Longitude - 1);
+                    return this.CheckPossibleOutsite(plateau, coordinate);
+                case CompassEnum.West:
+                    coordinate = new(this.Coordinate.Latitude - 1, this.Coordinate.Longitude);
+                    return this.CheckPossibleOutsite(plateau, coordinate);
+                default:
+                    return false;
+            }
+        }
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public bool CheckPossibleOutsite(Plateau plateau, Coordinate nextCoordinate)
+        {
+            Console.WriteLine($"nextCoordinate: {nextCoordinate.Longitude}");
+            Console.WriteLine($"plateau: {plateau.UpperRight.Longitude}");
+            if (nextCoordinate.Latitude > plateau.UpperRight.Latitude
+            || nextCoordinate.Longitude > plateau.UpperRight.Longitude)
+            {
+                Console.WriteLine("outside1");
+                return true;
+            }
+            if (nextCoordinate.Latitude < plateau.LowerLeft.Latitude
+            || nextCoordinate.Longitude < plateau.LowerLeft.Longitude)
+            {
+                Console.WriteLine("outside2");
+                return true;
+            }
+            return false;
+        }
+        public bool CheckPossibleCrash(Plateau plateau, Coordinate nextCoordinate)
+        {
+            foreach (var takenPoint in plateau.TakenPoints)
+            {
+                if (takenPoint.Latitude == nextCoordinate.Latitude
+                && takenPoint.Longitude == nextCoordinate.Longitude)
+                    return true;
+            }
+            return false;
         }
         public void TurnLeft()
         {
