0c0f37aa3542e5d806b731a286d8106b27c95eaa
Onur <onur.bolukbas@navlungo.com>
2021-07-05 18:19:20 +0300

    add taken points

--- Program/Models/Plateau.cs
+++ Program/Models/Plateau.cs
@@ -1,22 +1,22 @@
 using System;
+using System.Collections.Generic;
+
 namespace Program.Models
 {
     public class Plateau
     {
         public Coordinate LowerLeft { get; set; }
         public Coordinate UpperRight { get; set; }
+        public List<Coordinate> TakenPoints { get; set; }
 
         public Plateau(
             int Latitude,
             int Longitude
         )
         {
-            LowerLeft = new();
-            UpperRight = new();
-            this.UpperRight.Latitude = Latitude;
-            this.UpperRight.Longitude = Longitude;
-            this.LowerLeft.Latitude = 0;
-            this.LowerLeft.Longitude = 0;
+            UpperRight = new(Latitude, Longitude);
+            LowerLeft = new(0, 0);
+            TakenPoints = new();
         }
     }
 }
