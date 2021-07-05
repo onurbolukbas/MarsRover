bd28054e6f9b5e21d50bb44627487a5826f82d70
Onur <onur.bolukbas@navlungo.com>
2021-07-05 18:18:18 +0300

    update constructor

--- Program/Models/Coordinate.cs
+++ Program/Models/Coordinate.cs
@@ -4,5 +4,14 @@ namespace Program.Models
     {
         public int Latitude { get; set; }
         public int Longitude { get; set; }
+
+        public Coordinate(
+            int Latitude,
+            int Longitude
+        )
+        {
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+        }
     }
 }
