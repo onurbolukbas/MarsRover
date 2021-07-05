ecfc95373302570ac0ed4f5e6eb4e0f5dae0bde8
Onur <onur.bolukbas@navlungo.com>
2021-07-05 20:50:44 +0300

    add new helper

--- Program/Helpers/ProgramHelper.cs
+++ Program/Helpers/ProgramHelper.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Program.Models;
+using System;
 
 namespace Program.Helpers
 {
@@ -12,9 +14,29 @@ namespace Program.Helpers
             }
             return true;
         }
-        public static GetPlateauInformation(char[] plateauArr)
+        public static void ListRoverStates(List<Rover> roverList)
         {
+            foreach (var rover in roverList)
+            {
+                Console.WriteLine($"{rover.Name}: {rover.State.Coordinate.Latitude} {rover.State.Coordinate.Longitude} {rover.State.Direction}");
+            }
+        }
+        public static List<Rover> GenerateRover(string[] args)
+        {
+            List<Rover> roverList = new();
+            for (int i = 1; i < args.Length; i += 2)
+            {
+                char[] roverStateArr = args[i].ToCharArray();
+                string name = $"Rover - {i}";
 
+                Rover rover = new(name,
+                (int)Char.GetNumericValue(roverStateArr[0]),
+                (int)Char.GetNumericValue(roverStateArr[2]),
+                roverStateArr[4]);
+
+                roverList.Add(rover);
+            }
+            return roverList;
         }
     }
 }
